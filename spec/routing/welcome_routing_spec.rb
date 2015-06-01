99a2ea653c799a47d1757fb0c93bfc8e0eea2f0a
HariOm Gaur <hogaur@HariOms-MacBook-Pro.local>
2015-06-01 15:15:23 +0530

    Route spec for welcome#index added

--- spec/routing/welcome_routing_spec.rb
+++ spec/routing/welcome_routing_spec.rb
@@ -1,9 +1,7 @@
 require 'rails_helper'
 
-RSpec.describe WelcomeController, type: :routing do
-  describe 'routing' do
-    it 'should route to #index' do
-      expect(:get =>"/").to route_to("welcome#index")
-    end
+RSpec.describe "welcome route", type: :routing do
+  it 'should route / to welcome#index' do
+    expect(:get =>"/").to route_to("welcome#index")
   end
 end
